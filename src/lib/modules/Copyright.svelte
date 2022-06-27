7fa16f64ff6a1ce4e8bd9b0d053658ff081b1c51
Johnny Slack <1318041+johnny-cartelle@users.noreply.github.com>
2022-06-27 11:29:15 -0400

    Update Copyright.svelte

--- src/lib/modules/Copyright.svelte
+++ src/lib/modules/Copyright.svelte
@@ -1,10 +1,23 @@
-<div class="column-section">
-  <div />
-  <div class="copyright">©2022</div>
+<div class="column-section copyright-wrapper">
+  <div class="copyright light">©2022</div>
+  <div class="line" />
 </div>
 
 <style lang="scss">
-  .copyright {
+  @import "../../styles/mixins.scss";
+
+  .copyright-wrapper {
+    display: flex;
+    align-items: center;
     padding-bottom: 200px;
+    @include column-count("2") {
+      gap: 20px;
+    }
+  }
+
+  .line {
+    width: 100%;
+    height: 1px;
+    background-color: var(--color--light-grey);
   }
 </style>
